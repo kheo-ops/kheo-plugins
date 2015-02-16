f0316e65f44ed50edb40ca3fe66f63707891a0e0
Mikael Gibert <mikael.gibert@gmail.com>
2015-02-17 00:16:30 +0100

    Moving network interface code as plugin code

---
+++ network-interfaces/src/main/java/com/kheo/plugins/network/interfaces/NetworkInterfacePlugin.java
@@ -0,0 +1,22 @@
+package com.kheo.plugins.network.interfaces;
+
+import com.migibert.kheo.core.plugin.KheoPlugin;
+
+public class NetworkInterfacePlugin implements KheoPlugin<NetworkInterfaceServerProperty> {
+
+	@Override
+	public NetworkInterfaceCommand getSshCommand() {
+		return new NetworkInterfaceCommand();
+	}
+
+	@Override
+	public NetworkInterfaceEventGenerator getEventGenerator() {
+		return new NetworkInterfaceEventGenerator();
+	}
+	
+	@Override
+	public String getName() {
+		return NetworkInterfacePlugin.class.getSimpleName();
+	}
+
+}
